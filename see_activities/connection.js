3dd3ad0bd885c7b9d17daa39b170dc4d357b7cd8
Muhammad Rashel Mia <muhammadrashel2015@gmail.com>
2023-04-12 15:40:55 -0400

    see activities added

---
+++ see_activities/connection.js
@@ -0,0 +1,34 @@
+const url = 'https://coen6311-380422.nn.r.appspot.com/admin/report'
+const dataContainer = document.getElementById('see-acitivity');
+
+    
+fetch(url, {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({username:"Rashel111"})
+})
+.then(response => response.json())
+.then(data => {
+    displayAcivities(data);
+}) 
+
+
+function displayAcivities(data)
+{
+    //console.log(data);
+    let html = '';
+    data.forEach(item => {
+      html += `
+        <ul>
+          <li>Action: ${item.action}</li>
+          <li>Details: ${item.details}</li>
+          <li>TimeStamp: ${item.timestamp}</li>
+        </ul>
+      `;
+    });
+    console.log(data);
+    
+    dataContainer.innerHTML = html;
+}
