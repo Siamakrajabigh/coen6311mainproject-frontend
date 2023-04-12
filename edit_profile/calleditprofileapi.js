1488bdca2fd99f19ca2a1d0f23b06ddcdb686656
Shubha99 <shubha.sbj@gmail.com>
2023-04-12 14:29:54 -0400

    edit profile changes

--- edit_profile/calleditprofileapi.js
+++ edit_profile/calleditprofileapi.js
@@ -27,4 +27,80 @@ const form = document.getElementById("apiForm");
         });
       });
     
+      let username = localStorage.getItem('username');
+let userEmail = localStorage.getItem('userEmail');
+
+var data = {
+
+"email" : "userEmail"
+};
+
+const url = 'https://coen6311-380422.nn.r.appspot.com/findAUser'
+
+fetch(url, {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({email:userEmail})
+})
+.then(response => response.json())
+.then(data => {
+  displayData(data);
+})
+
+function displayData(data) {
+    // Get the HTML element with the ID 'api-data'
+    const apiDataElement = document.getElementById('personal-info');
+  
+    // Create a list item for the name and email
+    const li = document.createElement('li');
+    const fullName = `${data.firstName} ${data.lastName}`;
+    li.innerHTML = `Name: ${fullName}<br>Email: ${data.email}`;
+    apiDataElement.appendChild(li);
+  }
+ 
+  var data = {
+
+    "username":'username',
+    "myUsername": 'username'
+  };
+
+    var skills = [
+      {id: 1, skillTitle: 'Python'}
+    ]; 
+  
+  fetch('https://coen6311-380422.nn.r.appspot.com/findUserByUsername', {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({username:username, myUsername:username})
+  })
+  .then(response => response.json())
+  
+  .then(data =>
+    displayServiceProviderInfo(data)
+  )
+  
+  function displayServiceProviderInfo(data) {
+    const apiDataElement = document.getElementById('serviceprovider-info');
+    console.log(data);
+    // Create a list item for the name and email
+    const li = document.createElement('li');
+    li.innerHTML = `Name: ${data.username}`;
+    li.innerHTML = `Skills:`;
+
+    // Create a sub-list for the skills
+    const skillsList = document.createElement('ul');
+    data.skills.forEach(skill => {
+      const skillLi = document.createElement('li');
+      skillLi.textContent = skill.skillTitle;
+      skillsList.appendChild(skillLi);
+    });
+    li.appendChild(skillsList);
+  
+    apiDataElement.appendChild(li);
+  }
+  
     
