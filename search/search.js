e28c83eeabe8ecbae456023fa050d0ae8a3f7a91
Shubha99 <shubha.sbj@gmail.com>
2023-04-13 14:08:10 -0400

    search_sp

--- search/search.js
+++ search/search.js
@@ -102,9 +102,8 @@ fetch('https://coen6311-380422.nn.r.appspot.com/findUserByUsername', {
   {
     const dataContainer = document.getElementById('data-container');
 
-    const url = 'https://coen6311-380422.nn.r.appspot.com/viewAllNonTakenTickets'
     
-    fetch(url, {
+    fetch('https://coen6311-380422.nn.r.appspot.com/viewAllNonTakenTickets', {
         method: 'POST',
         headers: {
         'Content-Type': 'application/json'
@@ -202,29 +201,3 @@ fetch('https://coen6311-380422.nn.r.appspot.com/findUserByUsername', {
 })
 
 
-    
-    
-
-
-    // function displayTickets(data)
-    // {
-    //     //console.log(data);
-    //     let html = '';
-    //     data.forEach(item => {
-    //       html += `
-    //         <ul>
-    //           <li>ID: ${item.id}</li>
-    //           <li>Delivery Time: ${item.deliveryTime}</li>
-    //           <li>Task Type: ${item.taskType}</li>
-    //           <li>Required Skills: ${item.requiredSkills.join(', ')}</li>
-    //           <li>Technical Constraints: ${item.technicalConstraints}</li>
-    //           <li>Requirement Descriptions: ${item.requirementDescriptions}</li>
-    //           <li>End Users: ${item.endUsers.map(user => user.username).join(', ')}</li>
-    //           <li><button class="edit-button" data-id="${item.id}">Accept Ticket</button></li>
-    //         </ul>
-    //       `;
-    //     });
-    //     console.log(data);
-        
-    //     dataContainer.innerHTML = html;
-    // }
