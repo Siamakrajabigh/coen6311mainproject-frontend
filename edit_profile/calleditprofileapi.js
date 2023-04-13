e6eb5aa53615cd53056665a46044468965eb3174
Shubha99 <shubha.sbj@gmail.com>
2023-04-13 12:50:07 -0400

    no space in skills

--- edit_profile/calleditprofileapi.js
+++ edit_profile/calleditprofileapi.js
@@ -9,7 +9,7 @@ const form = document.getElementById("apiForm");
 
       const data = {
         "username": username,
-        "skills": form.requiredSkills.value.split(",")
+        "skills": form.requiredSkills.value.split(",").map(skill => skill.trim())
       };
 
     fetch("https://coen6311-380422.nn.r.appspot.com/updateServiceProviderSkills", {
@@ -23,7 +23,7 @@ const form = document.getElementById("apiForm");
     .then(response => response.json())
         .then(data => {
           console.log("API response:", data);
-          window.location.href = "https://www.greengrassfreelancer.com/profile_page/index.html";
+          //window.location.href = "https://www.greengrassfreelancer.com/profile_page/index.html";
         })
         .catch(error => {
           console.error("Error sending data:", error);
